a80786fc9ff41b18f645fe72ae2f027404011f30
bajobozic <bajobozic@gmail.com>
2020-07-25 20:31:56 +0200

    Capitalize message

--- src/App.js
+++ src/App.js
@@ -5,10 +5,10 @@ import "./App.css";
 function App() {
   return (
     <div className="app">
-      <Tweet name="One" message="I am number One" />
-      <Tweet name="Two" message="I am number Two" />
-      <Tweet name="Three" message="I am number Three" />
-      <Tweet name="Four" message="I am number Four" />
+      <Tweet name="One" message="I am number ONE" />
+      <Tweet name="Two" message="I am number TWO" />
+      <Tweet name="Three" message="I am number THREE" />
+      <Tweet name="Four" message="I am number FOUR" />
     </div>
   );
 }
